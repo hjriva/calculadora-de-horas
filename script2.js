982b2af5d8984725a37252414ff19a45a2df982b
hjriva <157857573+hjriva@users.noreply.github.com>
2024-03-30 18:40:39 -0300

    updates

--- script2.js
+++ script2.js
@@ -16,10 +16,15 @@ const MinAgora = window.document.getElementById("minuto").value.toString().padSt
 const DiaAgora = window.document.getElementById("dia").value.toString().padStart(2,'0')
 const MesAgora = window.document.getElementById("mes").value.toString().padStart(2,'0')
 const AnoAgora = window.document.getElementById("ano").value.toString()
+
 const agora = new Date(`${AnoAgora}-${MesAgora}-${DiaAgora}T${HorAgora}:${MinAgora}:00`)
 
+const dias = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"]
+const diadaSemana = dias[agora.getDay()]
+
 const AgoraFreq = agora.getTime()
-const Freq = Number(window.document.getElementById('horasInt').value) * 3600000
+const FreqNum = Number(window.document.getElementById('horasInt').value)
+const Freq = FreqNum  * 3600000
 const NumDias = Number(window.document.getElementById('numdias').value) * 86400000
 
 
@@ -28,11 +33,36 @@ const DataLimite = new Date(limit)
 //alert(limit)
 //alert (DataLimite)
 
-const Intervalos = []
+var limit2 = DataLimite.getTime()
 
 
+const Intervalos = [`${diadaSemana}, ${DiaAgora}/${MesAgora}/${AnoAgora}, ${HorAgora}:${MinAgora}`]
 
+QuantosIntervalos = ((limit2 - AgoraFreq) / 3600000) / FreqNum
+
+alert(QuantosIntervalos)
+
+for (var i = 1; i <= QuantosIntervalos ; i ++) {
+
+    const teste = AgoraFreq + Freq * i
+    const data = new Date(teste)
+    const minFor = data.getMinutes().toString().padStart(2,'0')
+    const horaFor = data.getHours().toString().padStart(2,'0')
+    const diaFor = data.getDate().toString().padStart(2,'0')
+    const mesFor = (data.getMonth()+1).toString().padStart(2,'0')
+    const yearFor = data.getFullYear()
+    const semanaFor = dias[data.getDay()]
+    const Intervalo = `${semanaFor}, ${diaFor}/${mesFor}/${yearFor}, ${horaFor}:${minFor }`
+    Intervalos.push(Intervalo)
     
+    }
+
+   
+
+
+    let str =  Intervalos.join("<br> ")
+    document.write(str)
+
 
 
 
